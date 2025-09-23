b5b28bea204df685b63ce8d510ae76ce5a21f73f
David Butt <dbutt@greenixpc.com>
2025-09-23 10:46:55 -0600

    init commit

---
+++ QuoteApi/Controllers/Quotes.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuoteApi.Controllers;
+
+[ApiController]
+public class Quotes : ControllerBase
+{
+    // GET
+    public string GetQuote()
+    {
+
+        return "";
+    }
+}
---
+++ QuoteApi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace QuoteApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
